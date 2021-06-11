ae0f99336c66af4e8d2524964cd9348909bbc3e1
Francesco Nora <francesco.nora@besharp.it>
2021-06-11 14:53:06 +0200

    feat: Added Releases section

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -13,7 +13,8 @@
                 twSlider: $('#tweetSlider'),
                 navAnchor: $("#navigation .anchor"),
                 supportForm: $("#support-form"),
-                contactsForm: $("#contacts-form")
+                contactsForm: $("#contacts-form"),
+                releases: $("#releases-content")
             },
             init: function () {
 
@@ -23,6 +24,7 @@
                 this._anchor();
                 this._accordion();
                 this._formValidate();
+                this._releases();
 
             },
             _hamburger: function () {
@@ -97,10 +99,10 @@
                 });
             },
             _formValidate: function () {
-                
-                var el = document.getElementById('g-recaptcha-response'); 
-                if (el) { 
-                    el.setAttribute('required', 'required'); 
+
+                var el = document.getElementById('g-recaptcha-response');
+                if (el) {
+                    el.setAttribute('required', 'required');
                 }
 
                 if (this.elements.supportForm.length > 0) {
@@ -136,7 +138,7 @@
                         errorPlacement: function (error, element) {
                             element.before(error);
                         },
-                        submitHandler: function() {
+                        submitHandler: function () {
                             form.addEventListener("submit", handleSubmit)
                         }
                     });
@@ -169,7 +171,7 @@
                         errorPlacement: function (error, element) {
                             element.before(error);
                         },
-                        submitHandler: function() {
+                        submitHandler: function () {
                             form.addEventListener("submit", handleSubmit)
                         }
                     });
@@ -195,11 +197,69 @@
                         status.classList.add("mystyle");
                         status.innerHTML = "<span class='error'> Oops! There was a problem submitting your form.</span>"
                     });
-                    setTimeout(function(){ 
+                    setTimeout(function () {
                         status.innerHTML = "";
                     }, 5000);
                 }
 
+            },
+            _releases: function () {
+
+                var _self = this;
+                var cfDistribution = "https://asset.noovolari.com";
+
+                $.ajax({
+                    type: "GET",
+                    url: cfDistribution + "/CHANGELOG.md",
+                    success: function (response) {
+
+                        var rawVersion = "0.0.0";
+                        var index = 1;
+
+                        var renderer = new marked.Renderer();
+
+                        renderer.heading = function (text, level, raw) {
+                            var version = text.replace(/[\(\[].*?[\)\]]/g, '').replace("\">", "\">v");
+
+                            if (level == 1)
+                                return '<h1>Changelog</h1>';
+                            else if (text.indexOf("Feat") > -1 | text.indexOf("Bug") > -1)
+                                return '<h' + level + ' class="italic">' + version + '</h' + level + '>';
+                            else {
+                                var context = "",
+                                    prefix = "";
+                                rawVersion = raw.replace(/[\(\[].*?[\)\]]/g, '').trim();
+                                if (text.indexOf("href") == -1) {
+                                    context = " class='nolink'";
+                                    prefix = "v ";
+                                }
+                                return '<h' + level + context + '>' + prefix + version + '</h' + level + '>';
+
+                            }
+                        };
+                        renderer.list = function (body) {
+                            var folder = index == 1 ? "latest" : rawVersion.trim();
+
+                            var generatedDownloadURL = body;
+                            generatedDownloadURL += '<ul class="download-list">';
+                                generatedDownloadURL += '<li><a href="' + cfDistribution + '/' + folder + '/Leapp-' + rawVersion + '-win.zip" class="download"><i class="fab fa-windows"></i> Download</a></li>';
+                                generatedDownloadURL += '<li><a href="' + cfDistribution + '/' + folder + '/Leapp-' + rawVersion + '-mac.zip" class="download"><i class="fab fa-apple"></i> Download</a></li>';
+                                generatedDownloadURL += '<li><a href="' + cfDistribution + '/' + folder + '/Leapp_' + rawVersion + '_amd64.deb" class="download"><i class="fab fa-linux"></i> Download</a></li>';
+                            generatedDownloadURL += '</ul>';
+
+                            index++;
+
+                            return generatedDownloadURL;
+                        }
+
+                        var responseMD = marked(response, {
+                            renderer: renderer,
+                            headerIds: false
+                        });
+
+                        _self.elements.releases.html(responseMD);
+                    }
+                });
             }
         };
 
---
+++ _pages/releases.markdown
@@ -0,0 +1,9 @@
+---
+title: Leapp - Releases
+description: Leapp - The best general purpose Credential generator in the world
+permalink: /releases
+layout: page
+hero_title: Releases
+hero_content: "releases.html"
+hero_image: 
+---
